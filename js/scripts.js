bde8acb4b7dd3153193b64ebd863b5af08cf508b
RitaWoki <ritapwanjiku@gmail.com>
2016-11-23 10:02:51 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+$("h1").click(function(){
+    alert("This is a header.");
+});
+
+$("p").click(function() {
+    alert("This is a paragraph.");
+});
+
+$("img").click(function() {
+    alert("This is an image.");
+});
+
+$("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you,this is a header.");
+
+});
+});
